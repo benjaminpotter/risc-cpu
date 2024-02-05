528c4d7ac4c810646c47ed327b609ef7db5b13f6
Ben <baffqd@gmail.com>
2024-02-05 09:34:17 -0500

    add registers

--- quartus/datapath.v
+++ quartus/datapath.v
@@ -9,12 +9,34 @@ module datapath(
 	input wire [31:0] register_in
 );
 
+wire pc_enable, ir_enable;
+wire [31:0] pc_data, ir_data;
+
 wire [31:0] ra_data, rb_data;
+
+wire mar_enable, mdr_enable;
+wire [31:0] mar_data, mdr_data;
+
+wire input_port_enable, output_port_enable;
+wire [31:0] input_port_data, output_port_data;
+
 wire [63:0] rz_data_in, rz_data_out;
+
+// control
+register pc(clear, clock, pc_enable, register_in, pc_data);
+register ir(clear, clock, ir_enable, register_in, ir_data);
 
 // register file
 register ra(clear, clock, register_select[0], register_in, ra_data);
 register rb(clear, clock, register_select[1], register_in, rb_data);
+
+// memory interface
+register mar(clear, clock, mar_enable, register_in, mar_data);
+register mdr(clear, clock, mdr_enable, register_in, mdr_data);
+
+// io interface
+register input_port(clear, clock, input_port_enable, register_in, input_port_data);
+register output_port(clear, clock, output_port_enable, register_in, output_port_data);
 
 register rz_lo(clear, clock, register_select[2], rz_data_in[31:0], rz_data_out[31:0]);
 register rz_hi(clear, clock, register_select[2], rz_data_in[63:32], rz_data_out[63:32]);
